5c734703e55d9996454011c0b737b1101545c517
bchalman <blakechalman@gmail.com>
2019-01-24 22:26:56 -0800

    Implement devise user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  # protect_from_forgery
+  # before_action :authenticate_user!
+
+  def new
+
+  end
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+
+end
